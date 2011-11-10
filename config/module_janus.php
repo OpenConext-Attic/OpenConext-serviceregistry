6d811b4e059f4b4a834ca500c449717e0bb26ed1
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2011-11-10 14:45:36 +0000

    changed metadata cron refresh to daily instead of hourly
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/trunk@1475 528793b8-127e-4309-9901-b74759c7208d

--- config/module_janus.php
+++ config/module_janus.php
@@ -472,7 +472,7 @@ $config = array(
         ),
     ),
 
-    'metadata_refresh_cron_tags'      => array('hourly'),
+    'metadata_refresh_cron_tags'      => array('daily'),
     'validate_entity_certificate_cron_tags' => array('daily'),
     'validate_entity_endpoints_cron_tags' => array('daily'),
     'ca_bundle_file' => '/etc/pki/tls/certs/ca-bundle.crt',
