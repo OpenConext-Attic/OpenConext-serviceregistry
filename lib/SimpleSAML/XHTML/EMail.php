852520b72c389eee59352bcbf802969dcd509459
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2011-11-15 15:38:34 +0000

    MERGE: Trunk to 1.13 branch BACKLOG-288 Metadata refresh cron jon will now run daily instead of hourly BACKLOG-287 Emails can now be sent with attachments BACKLOG-287 Added patch to make Janus sends notification emails instead of updating upon metadata change BACKLOG-238 Janus is now completely reverted before applying patches to prevent conflicts that automatic deployment cannot handle
    git-svn-id: https://svn.surfnet.nl/svn/coin-eb/serviceregistry/branches/serviceregistry-v1.13@1509 528793b8-127e-4309-9901-b74759c7208d

--- lib/SimpleSAML/XHTML/EMail.php
+++ lib/SimpleSAML/XHTML/EMail.php
@@ -9,6 +9,8 @@
  */
 class SimpleSAML_XHTML_EMail {
 
+    const BOUNDARY_OPEN = '--';
+    const BOUNDARY_CLOSE = '--';
 
 	private $to = NULL;
 	private $cc = NULL;
@@ -17,7 +19,9 @@ class SimpleSAML_XHTML_EMail {
 	private $replyto = NULL;
 	private $subject = NULL;
 	private $headers = array();
-	
+    private $_altBoundary;
+    private $_mixedBoundary;
+    private $_attachmentBody;
 
 	/**
 	 * Constructor
@@ -28,7 +32,19 @@ class SimpleSAML_XHTML_EMail {
 		$this->from = $from;
 		$this->replyto = $replyto;
 		$this->subject = $subject;
+        $this->_createBoundary();
 	}
+
+    /**
+     * Creates boundary string
+     *
+     * @return void
+     */
+    protected function _createBoundary() {
+        $random_hash = SimpleSAML_Utilities::stringToHex(SimpleSAML_Utilities::generateRandomBytes(16));
+        $this->_altBoundary = 'alt-' . $random_hash;
+        $this->_mixedBoundary = 'mixed-' . $random_hash;
+    }
 
 	function setBody($body) {
 		$this->body = $body;
@@ -66,30 +82,37 @@ pre {
 		if ($this->subject == NULL) throw new Exception('EMail field [subject] is required and not set.');
 		if ($this->body == NULL) throw new Exception('EMail field [body] is required and not set.');
 		
-		$random_hash = SimpleSAML_Utilities::stringToHex(SimpleSAML_Utilities::generateRandomBytes(16));
-		
+
 		if (isset($this->from))
 			$this->headers[]= 'From: ' . $this->from;
 		if (isset($this->replyto))
 			$this->headers[]= 'Reply-To: ' . $this->replyto;
 
-		$this->headers[] = 'Content-Type: multipart/alternative; boundary="simplesamlphp-' . $random_hash . '"'; 
-		
+        $this->headers[] = 'Content-Type: multipart/mixed; boundary="' . $this->_mixedBoundary . '"';
+
 		$message = '
---simplesamlphp-' . $random_hash . '
+' . self::BOUNDARY_OPEN . $this->_mixedBoundary . '
+Content-Type: multipart/alternative; boundary="' . $this->_altBoundary . '"
+
+' . self::BOUNDARY_OPEN . $this->_altBoundary . '
 Content-Type: text/plain; charset="utf-8" 
 Content-Transfer-Encoding: 8bit
 
 ' . strip_tags(html_entity_decode($this->body)) . '
 
---simplesamlphp-' . $random_hash . '
+' . self::BOUNDARY_OPEN . $this->_altBoundary . '
 Content-Type: text/html; charset="utf-8" 
 Content-Transfer-Encoding: 8bit
 
 ' . $this->getHTML($this->body) . '
 
---simplesamlphp-' . $random_hash . '--
+' . self::BOUNDARY_OPEN . $this->_altBoundary . self::BOUNDARY_CLOSE . '
 ';
+
+$message .= $this->_attachmentBody;
+
+$message .= self::BOUNDARY_CLOSE . $this->_mixedBoundary .self::BOUNDARY_CLOSE;
+
 		$headers = join("\r\n", $this->headers);
 
 		$mail_sent = @mail($this->to, $this->subject, $message, $headers);
@@ -97,6 +120,31 @@ Content-Transfer-Encoding: 8bit
 		if (!$mail_sent) throw new Exception('Error when sending e-mail');
 	}
 
-}
+    /**
+     * Add attachment from string
+     *
+     * @param   string $content
+     * @param   string    $name
+     * @return  void
+     *
+     * NOTE: This was added to solve Surfconext Backlog 287
+     * This might not be the best or safest way to add attachments
+     * It would be a very good idea to use something like Zend_Mail to replace this whole class
+     */
+    public function addAttachment($content, $name, $mimeType = 'application/octet-stream') {
+        // encode attachment content
+        $attachment = chunk_split(base64_encode($content));
 
+        $attachmentString = '
+--' . $this->_mixedBoundary . '
+Content-Type: ' . $mimeType . '; name="' . $name . '"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename="' . $name . '"
+
+' . $attachment . '
+';
+
+        $this->_attachmentBody .= $attachmentString;
+    }
+}
 ?>
