7d2f14df90ba589d0e63674617b005beca253cd0
Boy Baukema <boy.baukema@flex.surfnet.nl>
2012-06-26 14:05:46 +0200

    Upgrade JANUS to r1131 (fixing BACKLOG-505)

--- modules/janus/lib/AdminUtil.php
+++ modules/janus/lib/AdminUtil.php
@@ -11,7 +11,7 @@
  * @author     Sixto Martín, <smartin@yaco.es>
  * @copyright  2009 Jacob Christiansen
  * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @version    SVN: $Id: AdminUtil.php 1128 2012-06-19 11:20:15Z relaxnownl@gmail.com $
+ * @version    SVN: $Id: AdminUtil.php 1131 2012-06-26 12:02:48Z relaxnownl@gmail.com $
  * @link       http://code.google.com/p/janus-ssp/
  * @since      File available since Release 1.0.0
  */
@@ -30,7 +30,7 @@
  * @author     Sixto Martín, <smartin@yaco.es>
  * @copyright  2009 Jacob Christiansen
  * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @version    SVN: $Id: AdminUtil.php 1128 2012-06-19 11:20:15Z relaxnownl@gmail.com $
+ * @version    SVN: $Id: AdminUtil.php 1131 2012-06-26 12:02:48Z relaxnownl@gmail.com $
  * @link       http://code.google.com/p/janus-ssp/
  * @see        Sspmod_Janus_Database
  * @since      Class available since Release 1.0.0
@@ -559,7 +559,7 @@ class sspmod_janus_AdminUtil extends sspmod_janus_Database
             $remoteEids[] = $remoteEntity['eid'];
         }
 
-        $queryParams = array($entity->getEntityid(), $entity->getEntityid());
+        $queryParams = array($entity->getEid(), $entity->getEid());
         $queryParams = array_merge($queryParams, $remoteEids);
 
         $queryEidsIn = implode(', ', array_fill(0, count($remoteEids), '?'));
@@ -569,8 +569,8 @@ FROM (
     SELECT eid, entityid, revisionid, state, type, allowedall,
            (SELECT COUNT(*) > 0 FROM janus__allowedEntity WHERE je.eid = eid AND je.revisionid = revisionid) AS uses_whitelist,
            (SELECT COUNT(*) > 0 FROM janus__blockedEntity WHERE je.eid = eid AND je.revisionid = revisionid) AS uses_blacklist,
-           (SELECT COUNT(*) > 0 FROM janus__allowedEntity WHERE je.eid = eid AND je.revisionid = revisionid AND remoteentityid = ?) AS in_whitelist,
-           (SELECT COUNT(*) > 0 FROM janus__blockedEntity WHERE je.eid = eid AND je.revisionid = revisionid AND remoteentityid = ?) AS in_blacklist
+           (SELECT COUNT(*) > 0 FROM janus__allowedEntity WHERE je.eid = eid AND je.revisionid = revisionid AND remoteeid = ?) AS in_whitelist,
+           (SELECT COUNT(*) > 0 FROM janus__blockedEntity WHERE je.eid = eid AND je.revisionid = revisionid AND remoteeid = ?) AS in_blacklist
     FROM janus__entity je
     WHERE eid IN ($queryEidsIn)
       AND revisionid = (
