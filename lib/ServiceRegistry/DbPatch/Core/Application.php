e5d618342b5b92f8d1b719763a624e073eedc94a
Boy Baukema <boy@ibuildings.nl>
2012-06-27 12:14:58 +0200

    Set initial values if not set (useful because DbPatch wants a charset, by default this should be utf8)

--- lib/ServiceRegistry/DbPatch/Core/Application.php
+++ lib/ServiceRegistry/DbPatch/Core/Application.php
@@ -36,10 +36,11 @@ class ServiceRegistry_DbPatch_Core_Application extends DbPatch_Core_Application
             'db' => array(
                 'adapter'   => $this->_convertPdoDriverToZendDbAdapter($dsnParsed['scheme']),
                 'params' => array(
-                    'host'      => $dsnProperties['host'],
-                    'username'  => $dbConfig->username,
-                    'password'  => $dbConfig->password,
-                    'dbname'    => $dsnProperties['dbname'],
+                    'host'      => isset($dsnProperties['host'])    ? $dsnProperties['host']    : 'localhost',
+                    'username'  => isset($dbConfig->user)           ? $dbConfig->user           : 'root',
+                    'password'  => isset($dbConfig->password)       ? $dbConfig->password       : '',
+                    'dbname'    => isset($dsnProperties['dbname'])  ? $dsnProperties['dbname']  : 'serviceregistry',
+                    'charset'   => isset($dsnProperties['charset']) ? $dsnProperties['charset'] : 'utf8',
                 ),
             ),
             'patch_directory' => realpath(__DIR__ . self::PATCH_DIR_RELATIVE),
