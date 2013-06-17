fdc4363aae1242843f4462f20e4b3034b4dc12a7
lucasvanlierop <lucas@vanlierop.org>
2013-06-17 11:06:41 +0200

    Made patch script output more readable

--- bin/apply_janus_patches.sh
+++ bin/apply_janus_patches.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-echo -e "\nPatching files"
+echo -e "\nPatching files\n"
 REJFILE="/tmp/janus_patches-rej-$(date +%s)"
 touch $REJFILE
 for FILENAME in janus_patches/*.patch
 do
-  echo "$\nFILENAME\n\n"
+  echo "- Executing patch: $FILENAME\n"
   patch -Np1 -r $REJFILE < $FILENAME
+  echo "\n"
 done
 rm $REJFILE
 echo "Removed $REJFILE"
