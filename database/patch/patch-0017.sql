2ea32a125ac9b9d401533fd19b9e4b9e65bb5bd1
lucasvanlierop <lucas@vanlierop.org>
2013-07-12 11:27:40 +0200

    Added patch to change ip columns so that ipv6 addresses will fit (was already changed in janus)

---
+++ database/patch/patch-0017.sql
@@ -0,0 +1,25 @@
+-- Change ip columns so that ipv6 addresses will fit
+ALTER TABLE janus__allowedEntity
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__arp
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__attribute
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__blockedEntity
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__disableConsent
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__entity
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__hasEntity
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__message
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__metadata
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__subscription
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__user
+	CHANGE ip ip char(39) NOT NULL;
+ALTER TABLE janus__userData
+	CHANGE ip ip char(39) NOT NULL;
