44413e860b70b5c9cac9d5eae87b4efdc24fcce7
lucasvanlierop <lucas@vanlierop.org>
2014-02-14 15:32:47 +0100

    Remove usesless test

--- tests/DefaultTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DefaultTestCase extends PHPUnit_Framework_TestCase
-{
-    public function testNothing()
-    {
-        $this->assertTrue(true, "All is right with the universe");
-    }
-}
